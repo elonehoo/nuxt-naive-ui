112ed36eadfc974b0d52b6ff7c71e5ede24de48e
elonehoo <hi@elonehoo.me>
2023-02-14 21:33:28 +0800

    feat: update nuxt and ui version

--- src/module.ts
+++ src/module.ts
@@ -21,7 +21,7 @@ export default defineNuxtModule<ModuleOptions>({
     version,
     configKey: 'naiveUI',
     compatibility: {
-      nuxt: '^3.0.0',
+      nuxt: '^3.2.0',
     },
   },
   setup(moduleOptions, nuxt) {
