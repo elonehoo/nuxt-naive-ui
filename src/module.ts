8e7a5151bd5171b36664d7c6edae7a8175e4957e
elonehoo <hi@elonehoo.me>
2023-03-26 09:45:36 +0800

    chore: update all non-major dependencies

--- src/module.ts
+++ src/module.ts
@@ -21,7 +21,7 @@ export default defineNuxtModule<ModuleOptions>({
     version,
     configKey: 'naiveUI',
     compatibility: {
-      nuxt: '^3.2.0',
+      nuxt: '^3.3.2',
     },
   },
   setup(moduleOptions, nuxt) {
